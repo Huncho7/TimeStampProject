a6ba22a9a9ea25f4978289235fed2d40d76163d2
Victor Oluborode <oluborodevictor110@gmail.com>
2024-07-18 10:58:01 +0100

    new feature

--- index.js
+++ index.js
@@ -1,3 +1,98 @@
+// // index.js
+// // where your node app starts
+
+// // init project
+// var express = require('express');
+// var app = express();
+
+// // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// // so that your API is remotely testable by FCC 
+// var cors = require('cors');
+// app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// // http://expressjs.com/en/starter/static-files.html
+// app.use(express.static('public'));
+
+// // http://expressjs.com/en/starter/basic-routing.html
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+
+
+// // your first API endpoint... 
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
+
+// app.get("/api/", (req, res) => {
+//   let currentTime = new Date();
+//   let currentTimeStamp = Math.floor(currentTime.getTime());
+//   let currentUtcTime = currentTime.toUTCString();
+//   console.log(currentTime, currentTimeStamp, currentUtcTime);
+//   res.json({
+//     unix: currentTimeStamp,
+//     utc: currentUtcTime
+//   })
+
+// })
+
+// app.get("/api/:date", (req, res) => {
+//   // res.send("Hello");
+
+//     // To test for string validity
+
+//     let datePattern = /^\d{4}-\d{1,2}-\d{1,2}$/;
+//     let numericPattern = /^\d+$/;
+
+//     // When date string is valid
+
+//     if (
+//       datePattern.test(req.params.date) === true ||
+//       numericPattern.test(req.params.date)
+//     ) {
+//       // Getting date from url
+//       let date = req.params.date;
+
+//       if (!date.includes("-")) {
+//         date = Number(date);
+//       }
+
+//       // date = Number(date);
+//       const newDate = new Date(date);
+//       console.log(newDate);
+      
+//       // Timestamp function
+//       const timeStampDate = Math.floor(newDate.getTime());
+//       console.log(timeStampDate);
+
+//       // UTC date
+//       const utcDate = newDate.toUTCString();
+//       console.log(utcDate);
+
+//       // Response object
+//       res.json({
+//         unix: timeStampDate,
+//         utc: utcDate,
+//       });
+
+//       // When date string is invalid 
+//     } else {
+//       res.json({
+//         error: "Invalid date",
+//       });
+//     }
+//   }
+// );
+
+
+
+
+// // Listen on port set in environment variable or default to 3000
+// var listener = app.listen(process.env.PORT || 3000, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+
 // index.js
 // where your node app starts
 
@@ -18,7 +113,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
@@ -28,64 +122,32 @@ app.get("/api/", (req, res) => {
   let currentTime = new Date();
   let currentTimeStamp = Math.floor(currentTime.getTime());
   let currentUtcTime = currentTime.toUTCString();
-  console.log(currentTime, currentTimeStamp, currentUtcTime);
   res.json({
     unix: currentTimeStamp,
     utc: currentUtcTime
-  })
-
-})
+  });
+});
 
 app.get("/api/:date", (req, res) => {
-  // res.send("Hello");
+  let dateString = req.params.date;
 
-    // To test for string validity
-
-    let datePattern = /^\d{4}-\d{1,2}-\d{1,2}$/;
-    let numericPattern = /^\d+$/;
-
-    // When date string is valid
-
-    if (
-      datePattern.test(req.params.date) === true ||
-      numericPattern.test(req.params.date)
-    ) {
-      // Getting date from url
-      let date = req.params.date;
-
-      if (!date.includes("-")) {
-        date = Number(date);
-      }
-
-      // date = Number(date);
-      const newDate = new Date(date);
-      console.log(newDate);
-      
-      // Timestamp function
-      const timeStampDate = Math.floor(newDate.getTime());
-      console.log(timeStampDate);
-
-      // UTC date
-      const utcDate = newDate.toUTCString();
-      console.log(utcDate);
-
-      // Response object
-      res.json({
-        unix: timeStampDate,
-        utc: utcDate,
-      });
-
-      // When date string is invalid 
-    } else {
-      res.json({
-        error: "Invalid date",
-      });
-    }
+  // If the dateString is purely numeric, treat it as a timestamp
+  if (/^\d+$/.test(dateString)) {
+    dateString = parseInt(dateString);
   }
-);
 
+  const newDate = new Date(dateString);
 
-
+  // Check if the date is valid
+  if (isNaN(newDate.getTime())) {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: newDate.getTime(),
+      utc: newDate.toUTCString()
+    });
+  }
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
